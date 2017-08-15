2dd60f8e91f4973cafbff14955b7a4cd6a616313
alex-Arc <alex@arc-light.dk>
2017-08-15 22:11:04 +0200

    remove loop and setup

--- FirmwareFlasher.cpp
+++ FirmwareFlasher.cpp
@@ -9,46 +9,6 @@
 // #define Serial_Read()       Serial.read()
 // #define Serial_Printf       Serial.printf()
 
-const int ledPin = 13;
-
-void setup ()
-{
-  Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);    // set the LED on
-  delay(2000);
-  Serial.println("hey!!!!!!!");
-  // put your setup code here, to run once:
-  boot_check();             // check if we need to upgrade firmware before running loop()
-  digitalWrite(ledPin, LOW);     // set the LED off
-}
-
-void loop ()
-{
-  //simple example of writing to flash
-  /*
-  int ret;
-  uint32_t addr = FLASH_SIZE / 2;
-  ret = flash_word (addr, 0x123);
-  Serial.printf ("%x:%x  %d\n", addr, *(volatile unsigned int *) addr, ret);
-  ret = flash_word (addr, 0x456);  // this will fail
-  Serial.printf ("%x:%x  %d\n", addr, *(volatile unsigned int *) addr, ret);
-  flash_erase_sector (addr, 0);
-  ret = flash_word (addr, 0x456);
-  Serial.printf ("%x:%x  %d\n", addr, *(volatile unsigned int *) addr, ret);
-  */
-
-  // upgrade_firmware();
-
-  // for (;;) {}
-
-  digitalWrite(ledPin, HIGH);    // set the LED on
-  delay(100);
-  digitalWrite(ledPin, LOW);     // set the LED off
-  delay(100);
-
-}  // loop()
-
 
 // Example Serial_Printf() if needed as a reference.  With this, you need to write Serial_Print() (probably easier).
 /*#define SIZE 200
