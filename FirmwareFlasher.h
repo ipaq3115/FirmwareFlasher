a41043fbfbb16da6d75d42b030095b68cfea2d3a
alex-Arc <alex@arc-light.dk>
2017-08-17 11:20:36 +0200

    add T_3.5 and 3.6 def

--- FirmwareFlasher.h
+++ FirmwareFlasher.h
@@ -5,16 +5,26 @@
 #include <kinetis.h>
 //#include "serial.h"
 
-#if defined(__MK20DX128__)
+#if defined(__MK20DX128__) //T_3.0
 #define FLASH_SIZE              0x20000
 #define FLASH_SECTOR_SIZE       0x400
 #define FLASH_ID                "fw_teensy3.0"
 // TODO: find a way to force the compiler to not optimize FLASH_ID out
 #define RESERVE_FLASH (2 * FLASH_SECTOR_SIZE)
-#elif defined(__MK20DX256__)
+#elif defined(__MK20DX256__)//T_3.1 and T_3.2
 #define FLASH_SIZE              0x40000
 #define FLASH_SECTOR_SIZE       0x800
 #define FLASH_ID                "fw_teensy3.1"
+#define RESERVE_FLASH (1 * FLASH_SECTOR_SIZE)
+#elif defined(__MK64FX512__)//T_3.5
+#define FLASH_SIZE              0x80000
+#define FLASH_SECTOR_SIZE       0x1000
+#define FLASH_ID                "fw_teensy3.5"
+#define RESERVE_FLASH (1 * FLASH_SECTOR_SIZE)
+#elif defined(__MK66FX1M0__)//T_3.6
+#define FLASH_SIZE              0x100000
+#define FLASH_SECTOR_SIZE       0x1000
+#define FLASH_ID                "fw_teensy3.6"
 #define RESERVE_FLASH (1 * FLASH_SECTOR_SIZE)
 #else
 #error NOT SUPPORTED
